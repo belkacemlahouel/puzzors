f4e41b5a617e2b6c63d7dc5da6c9e2cceaf9a5d5
Donatien Rabiller <rabiller.donatien@hotmail.fr>
2015-10-04 14:49:14 -0400

    0.1 : Initial Commit.

---
+++ Source/Puzzors/Puzzors.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Puzzors.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Puzzors, "Puzzors" );
